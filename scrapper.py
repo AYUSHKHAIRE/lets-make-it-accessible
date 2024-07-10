96e95bc560f5d0be812798fe32c999f85de2bd25
AYUSH KHAIRE <141166916+AYUSHKHAIRE@users.noreply.github.com>
2024-07-10 17:55:43 +0530

    Update scrapper.py

--- scrapper.py
+++ scrapper.py
@@ -7,7 +7,10 @@ import time
 def get_response(url):
     print(url)
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Connection': 'keep-alive'
     }
     try:
         response = requests.get(url, headers=headers)

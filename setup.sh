37e46e1b55c7c9390153767c7ebd66f1ad6f2946
AYUSH KHAIRE <141166916+AYUSHKHAIRE@users.noreply.github.com>
2024-07-10 17:14:30 +0530

    adding setup.sh

---
+++ setup.sh
@@ -0,0 +1,23 @@
+# setup.sh
+
+# Install Google Chrome
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+sudo apt-get update
+sudo apt-get install -y google-chrome-stable
+
+# Install ChromeDriver
+CHROME_VERSION=$(google-chrome --version | awk '{print $3}')
+CHROMEDRIVER_VERSION=$(curl -s "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$CHROME_VERSION")
+wget -N https://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+sudo mv chromedriver /usr/local/bin/chromedriver
+sudo chmod +x /usr/local/bin/chromedriver
+
+# Clean up
+rm chromedriver_linux64.zip
+
+# Set environment variables
+echo "export GOOGLE_CHROME_BIN=/usr/bin/google-chrome" >> ~/.bashrc
+echo "export CHROMEDRIVER_PATH=/usr/local/bin/chromedriver" >> ~/.bashrc
+source ~/.bashrc

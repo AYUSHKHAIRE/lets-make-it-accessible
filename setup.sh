a2a121add5fca1320b8232150dc9e781b3e71dd7
AYUSH KHAIRE <141166916+AYUSHKHAIRE@users.noreply.github.com>
2024-07-10 17:17:42 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
-# setup.sh
+#!/bin/bash
+
+# Update package list and install dependencies
+sudo apt-get update
+sudo apt-get install -y wget unzip
 
 # Install Google Chrome
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
@@ -7,7 +11,7 @@ sudo apt-get update
 sudo apt-get install -y google-chrome-stable
 
 # Install ChromeDriver
-CHROME_VERSION=$(google-chrome --version | awk '{print $3}')
+CHROME_VERSION=$(google-chrome --version | grep -oP '[0-9.]+' | head -1)
 CHROMEDRIVER_VERSION=$(curl -s "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$CHROME_VERSION")
 wget -N https://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
